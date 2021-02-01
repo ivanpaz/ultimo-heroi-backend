013dea85ba9f2cb8d32bfe1ba2fb2bbaeccf5bcd
ivanpazl2code <ivan.paz@l2code.com.br>
2021-02-01 02:40:32 -0300

    a

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
-/*const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv/config');
 
-mongoose.connect(process.env.MONGODB_URL, {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser:true});
 mongoose.Promise = global.Promise;
 
-module.exports = mongoose;*/
+module.exports = mongoose;
