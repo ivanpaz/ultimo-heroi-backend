1f33863831174fc595bbd08b408bc0d3ed6d98bb
Bugan <dones.bugan@gmail.com>
2021-03-05 13:02:11 -0300

    passando eslint

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,26 +1,26 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-const express = require('express');
+const express = require("express");
 
-const Team = require('../models/team');
+const Team = require("../models/team");
 //const Admin = require('../models/admin');
 
 module.exports = {
 
-    async login(request, response){
-        console.log("Tentar logar",request.body.matricula, request.body.password );
-        const time = await Team.findOne({matricula: request.body.matricula, password: request.body.password});            
+	async login(request, response){
+		console.log("Tentar logar",request.body.matricula, request.body.password );
+		const time = await Team.findOne({matricula: request.body.matricula, password: request.body.password});            
         
-        console.log(time);
-        if(time != undefined){
-            return response.json(time);
-        }else{ 
-            console.log("Senha ou matricula incorreto");
-            return response.status(400).json({error: 'Senha ou matricula incorrentos'});
-        }
-    },
+		console.log(time);
+		if(time != undefined){
+			return response.json(time);
+		}else{ 
+			console.log("Senha ou matricula incorreto");
+			return response.status(400).json({error: "Senha ou matricula incorrentos"});
+		}
+	},
 
-   /* async admin(request, response){
+	/* async admin(request, response){
         console.log("Tentar logar ssss",request.body.user, request.body.password );
         const time = await Admin.findOne({user: request.body.user, password: request.body.password});            
         
@@ -33,12 +33,12 @@ module.exports = {
         }
     },*/
 
-    async listAdmin(request, response){
-        console.log("Lista de admins");
+	async listAdmin(request, response){
+		console.log("Lista de admins");
 
-        const times = await Team.find({});            
-        return response.json(times);
-    },
+		const times = await Team.find({});            
+		return response.json(times);
+	},
 
     
-}
+};
--- src/models/admin.js
+++ src/models/admin.js
@@ -2,54 +2,54 @@ const mongoose = require("../database");
 
 const AdmSchema = new mongoose.Schema({
 
-    user:{
-        type: String,
-        required: true
-    },
+	user:{
+		type: String,
+		required: true
+	},
    
-    password:{
-        type: String,
-        required: true
-    },   
-    m1:{
-        type: Number
-    },
-    m2:{
-        type: Number
-    },
-    m3:{
-        type: Number
-    },
-    m4:{
-        type: Number
-    },
-    m5:{
-        type: Number
-    },
-    m6:{
-        type: Number
-    },
-    m7:{
-        type: Number
-    },
-    m8:{
-        type: Number
-    },
-    m9:{
-        type: Number
-    },
-    m10:{
-        type: Number
-    },
-    m11:{
-        type: Number
-    },
-    m12:{
-        type: Number
-    }
+	password:{
+		type: String,
+		required: true
+	},   
+	m1:{
+		type: Number
+	},
+	m2:{
+		type: Number
+	},
+	m3:{
+		type: Number
+	},
+	m4:{
+		type: Number
+	},
+	m5:{
+		type: Number
+	},
+	m6:{
+		type: Number
+	},
+	m7:{
+		type: Number
+	},
+	m8:{
+		type: Number
+	},
+	m9:{
+		type: Number
+	},
+	m10:{
+		type: Number
+	},
+	m11:{
+		type: Number
+	},
+	m12:{
+		type: Number
+	}
 
 });
 
-const Adm = mongoose.model('Adm', AdmSchema);
+const Adm = mongoose.model("Adm", AdmSchema);
 
 module.exports = Adm;
